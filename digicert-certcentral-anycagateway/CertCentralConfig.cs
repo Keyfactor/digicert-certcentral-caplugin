ac791a7563dce2ebc297694d0c4569b24b9ab545
David Galey <dgaley@keyfactor.com>
2024-04-19 02:10:27 -0400

    Port sync CA filter from DCOM gateway

--- digicert-certcentral-anycagateway/CertCentralConfig.cs
+++ digicert-certcentral-anycagateway/CertCentralConfig.cs
@@ -8,10 +8,18 @@ namespace Keyfactor.Extensions.CAGateway.DigiCert
 {
 	public class CertCentralConfig
 	{
+
+		public CertCentralConfig()
+		{
+			SyncCAFilter = new List<string>();
+		}
 		public string APIKey { get; set; }
 		public string Region { get; set; } = "US";
 		public int? DivisionId { get; set; }
 		public bool? RevokeCertificateOnly { get; set; }
 		public bool Enabled { get; set; } = true;
+		public List<string> SyncCAFilter { get; set; }
+		public bool? FilterExpiredOrders { get; set; }
+		public int? SyncExpirationDays { get; set; }
 	}
 }
